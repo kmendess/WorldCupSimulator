82c51666de82a3b8bd5c4bee8107af572b7c760c
kmendess <karolmendes.rj@gmail.com>
2022-12-04 00:33:28 -0300

    Added Cors and new endpoint to get teams grouped by groups.

--- World.Cup.Simulator/World.Cup.Simulator/Program.cs
+++ World.Cup.Simulator/World.Cup.Simulator/Program.cs
@@ -3,6 +3,7 @@ using World.Cup.Simulator;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 builder.Services.AddDbContext<WorldCupContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConnection")));
 builder.Services.AddEndpointsApiExplorer();
@@ -15,21 +16,36 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/teams/{id}", async (WorldCupContext context, Guid id) =>
+app.UseCors(p => p
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+
+app.MapGet("/api/teams/groups", async (WorldCupContext context) =>
+{
+    var teams = await context.Teams.ToListAsync();
+    var groups = teams.GroupBy(p => p.Group)
+                    .OrderBy(p => p.Key)
+                    .Select(p => p.Select(p => p));
+
+    return Results.Ok(groups);
+});
+
+app.MapGet("/api/teams/{id}", async (WorldCupContext context, Guid id) =>
 {
     var team = await context.Teams.FindAsync(id);
 
     return Results.Ok(team);
 });
 
-app.MapGet("/teams", async (WorldCupContext context) =>
+app.MapGet("/api/teams", async (WorldCupContext context) =>
 {
     var teams = await context.Teams.ToListAsync();
 
     return Results.Ok(teams);
 });
 
-app.MapPost("/teams", async (WorldCupContext context, Team team) =>
+app.MapPost("/api/teams", async (WorldCupContext context, Team team) =>
 {
     await context.Teams.AddAsync(team);
     await context.SaveChangesAsync();
@@ -37,7 +53,7 @@ app.MapPost("/teams", async (WorldCupContext context, Team team) =>
     return Results.Ok(team);
 });
 
-app.MapPut("/teams", async (WorldCupContext context, Team team) =>
+app.MapPut("/api/teams", async (WorldCupContext context, Team team) =>
 {
     var dbTeam = await context.Teams.FindAsync(team.Id);
     if (dbTeam == null)
@@ -52,7 +68,7 @@ app.MapPut("/teams", async (WorldCupContext context, Team team) =>
     return Results.Ok(dbTeam);
 });
 
-app.MapDelete("/teams/{id}", async (WorldCupContext context, Guid id) =>
+app.MapDelete("/api/teams/{id}", async (WorldCupContext context, Guid id) =>
 {
     var dbTeam = await context.Teams.FindAsync(id);
     if (dbTeam == null)
@@ -71,4 +87,5 @@ public class Team
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Img { get; set; }
+    public int Group { get; set; }
 }
